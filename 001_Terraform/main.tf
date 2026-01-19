79256db2490308509074027bfd337fb6bfe2a2b2
Carsano <damien.fabiano@protonmail.com>
2026-01-19 20:26:30 +0100

    Add main.tf and update gitignore(

--- 001_Terraform/main.tf
+++ 001_Terraform/main.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "7.16.0"
     }
   }
 }
 
 provider "google" {
-  # Configuration options
+  project = "dtc-de-course-484507"
+  region  = "europe-west9"
 }
