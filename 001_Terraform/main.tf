bd68775765ad2021397f18d567dd23fc4d42a9a1
Carsano <damien.fabiano@protonmail.com>
2026-01-19 20:49:27 +0100

    Create terraform

--- 001_Terraform/main.tf
+++ 001_Terraform/main.tf
@@ -8,6 +8,30 @@ terraform {
 }
 
 provider "google" {
-  project = "dtc-de-course-484507"
-  region  = "europe-west9"
+  project     = "dtc-de-course-484507"
+  region      = "europe-west9"
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
